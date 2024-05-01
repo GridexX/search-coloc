72f4579632cefb48effa283ecfd6e9525b91f528
Arsène Fougerouse <arsene582@gmail.com>
2024-05-01 15:32:36 +0200

    feat: filter, excel and search on last email
    Signed-off-by: Arsène Fougerouse <arsene582@gmail.com>

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const process = require('process');
 const { authenticate } = require('@google-cloud/local-auth');
 const { google } = require('googleapis');
 const axios = require('axios');
+const { firstBy } = require('thenby');
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
@@ -93,8 +94,163 @@ async function calculateTravelTime(origin, destination, mode) {
   } catch (error) {
     console.error(error);
   }
-
   return -1;
+
+}
+
+function sortApartments(apartments) {
+
+  const sortedApartments = apartments.toSorted(
+    firstBy((a, b) => (a.travelTimeBikeToPolytech + a.travelTimeBikeToComedie) - (b.travelTimeBikeToPolytech + b.travelTimeBikeToComedie))
+      .thenBy((a, b) => a.rent - b.rent)
+  );
+
+  // const sortedApartments = apartments.toSorted((a, b) => {
+  //   // aTravelTime is the sum of the travel time to Polytech and Comedie or undifined if the travel time couldn't be calculated
+  //   const aTravelTime = a.travelTimeBikeToPolytech + a.travelTimeBikeToComedie || 0;
+  //   const bTravelTime = b.travelTimeBikeToPolytech + b.travelTimeBikeToComedie || 0;
+  //   return aTravelTime - bTravelTime;
+  // });
+
+
+  return sortedApartments;
+
+}
+
+function getShortenedUrl(url) {
+  const parsedUrl = new URL(url);
+
+  // Extract the pathname component
+  const pathname = parsedUrl.pathname;
+
+  // Retrieve the second link
+  const encodedLink = pathname.split('L0/')[1];
+
+  // Decode the URL-encoded string
+  const decodedLink = decodeURIComponent(encodedLink);
+  const reduceLink = decodedLink.split('/a/')[1].split('/1/')[0];
+
+  const apartmentNumberMatch = reduceLink.match(/[a-zA-Z0-9]{6}/)
+
+  if (!apartmentNumberMatch) {
+    throw new Error('The reduce link does not match the expected format.');
+  }
+
+  const shortenedUrl = `https://lcrt.fr/${apartmentNumberMatch[0]}`
+
+  return shortenedUrl;
+}
+
+async function getApartments(listings) {
+
+  // Loop through the matched listings and extract the information from the td elements
+  const apartments = [];
+  for (const listing of listings) {
+    const $ = cheerio.load(listing);
+    const tds = $('td');
+
+    const apartment = {};
+
+    // Do something with the tds, for example print the text content to the console
+    for (const td of tds) {
+      // Find all h3 elements
+      let isTitle = false;
+      const h3s = $(td).find('h3');
+      const h4s = $(td).find('h4');
+      if (h3s.length && h3s.length > 0) {
+        isTitle = true;
+        for (const h3 of h3s) {
+
+
+          // If the H3 Match number, we extract and assign it as the rent
+          const rentMatch = $(h3).text().match(/^(\d+)$/);
+          if (rentMatch) {
+            apartment.rent = parseInt(rentMatch[1]);
+          } else {
+            // If the H3 doesn't match a number, we assume it's the title
+            apartment.title = $(h3).text();
+
+            // Extract the number of rooms from the title
+            const roomMatch = $(h3).text().match(/.*(\d+) pièces.*/);
+            if (roomMatch) {
+              apartment.rooms = parseInt(roomMatch[1]);
+            }
+          }
+
+          console.log(`H3: ${$(h3).text()}`);
+        }
+      }
+      if (h4s.length && h4s.length > 0) {
+        isTitle = true;
+        for (let index = 0; index < h4s.length; index++) {
+          const h4 = h4s[index];
+          const roomMatch = $(h4).text().match(/.*chambre de (\d+).*/);
+          if (roomMatch) {
+            // Convert the room surface to a number
+            apartment.roomSurface = parseInt(roomMatch[1]);
+          } else if (index === 1) {
+
+            const streetMatch = $(h4).text().match(/.*, (.*)/);
+            if (streetMatch) {
+              apartment.street = streetMatch[1];
+
+              // If the street is specified, calculate the travel time by bike
+              // TODO Add severall adresses to calculate the travel time to
+              // With a JSON file containing the adresses
+              const comedieAdress = 'Place de la Comedie, Montpellier, France';
+              const polytechAdress = 'Place Eugene Bataillon, Montpellier, France';
+              const destination = `${apartment.street}, Montpellier, France`;
+              const mode = 'bicycling';
+
+              const travelTimeBikeToComedie = await calculateTravelTime(comedieAdress, destination, mode);
+              const travelTimeBikeToPolytech = await calculateTravelTime(polytechAdress, destination, mode);
+
+              apartment.travelTimeBikeToComedie = travelTimeBikeToComedie;
+              apartment.travelTimeBikeToPolytech = travelTimeBikeToPolytech;
+            }
+          }
+        }
+        for (const h4 of h4s) {
+          console.log(`H4: ${$(h4).text()}`);
+        }
+      }
+
+      // Get the href from the a element inside the td
+      const a = $(td).find('a');
+      const href = a.attr('href');
+      const text = $(td).text().trim().replace(/\s+/g, ' ');
+      if (!isTitle && href && href.length > 0) {
+        console.log("p: " + href);
+        apartment.link = getShortenedUrl(href);
+      } else if (!isTitle && text && text.length > 0) {
+        apartment.description = text;
+      }
+    }
+    apartments.push(apartment);
+    console.log();
+  }
+  console.log(apartments);
+
+  return apartments;
+}
+
+/**
+ * This function filters the apartments based on the user's preferences.
+ */
+function filterApartments(apartments) {
+
+  const maxRooms = 5; // 4 roommates and the living room
+  const minSurface = 10; // 10 m2 for the room
+
+  const maxTravelComedie = 12; // 12 minutes by bike to Comedie
+  const maxTravelPolytech = 18; // 18 minutes by bike to Polytech
+
+  return apartments.filter(apartment => {
+    return apartment.rooms <= maxRooms &&
+      apartment.roomSurface >= minSurface &&
+      apartment.travelTimeBikeToComedie <= maxTravelComedie &&
+      apartment.travelTimeBikeToPolytech <= maxTravelPolytech;
+  })
 
 }
 
@@ -129,100 +285,36 @@ async function listMessages(auth) {
         const html = Buffer.from(body.data, 'base64url').toString('utf-8');
         // console.log(decodedData);
         const listingRegex = /<!-- BEGIN LISTING \/\/ -->(.*?)<!-- \/\/ END LISTING -->/gs;
+
         const listings = html.match(listingRegex);
         if (!listings) {
           console.log('No listings found.');
-          return;
+          return [];
         }
-        // Loop through the matched listings and extract the information from the td elements
-        const apartments = [];
-        for (const listing of listings) {
-          const $ = cheerio.load(listing);
-          const tds = $('td');
 
-          const apartment = {};
-
-          // Do something with the tds, for example print the text content to the console
-          for (const td of tds) {
-            // Find all h3 elements
-            let isTitle = false;
-            const h3s = $(td).find('h3');
-            const h4s = $(td).find('h4');
-            if (h3s.length && h3s.length > 0) {
-              isTitle = true;
-              for (const h3 of h3s) {
-
-
-                // If the H3 Match number, we extract and assign it as the rent
-                const rentMatch = $(h3).text().match(/^(\d+)$/);
-                if (rentMatch) {
-                  apartment.rent = parseInt(rentMatch[1]);
-                } else {
-                  // If the H3 doesn't match a number, we assume it's the title
-                  apartment.title = $(h3).text();
-                }
-
-                console.log(`H3: ${$(h3).text()}`);
-              }
-            }
-            if (h4s.length && h4s.length > 0) {
-              isTitle = true;
-              for (let index = 0; index < h4s.length; index++) {
-                const h4 = h4s[index];
-                const roomMatch = $(h4).text().match(/.*chambre de (\d+).*/);
-                if (roomMatch) {
-                  // Convert the room surface to a number
-                  apartment.roomSurface = parseInt(roomMatch[1]);
-                } else if (index === 1) {
-
-                  const streetMatch = $(h4).text().match(/.*, (.*)/);
-                  if (streetMatch) {
-                    apartment.street = streetMatch[1];
-
-                    // If the street is specified, calculate the travel time by bike
-                    // TODO Add severall adresses to calculate the travel time to
-                    // With a JSON file containing the adresses
-                    const comedieAdress = 'Place de la Comedie, Montpellier, France';
-                    const polytechAdress = 'Place Eugene Bataillon, Montpellier, France';
-                    const destination = `${apartment.street}, Montpellier, France`;
-                    const mode = 'bicycling';
-
-                    const travelTimeBikeToComedie = await calculateTravelTime(comedieAdress, destination, mode);
-                    const travelTimeBikeToPolytech = await calculateTravelTime(polytechAdress, destination, mode);
-
-                    apartment.travelTimeBikeToComedie = travelTimeBikeToComedie;
-                    apartment.travelTimeBikeToPolytech = travelTimeBikeToPolytech;
-
-                  }
-                }
-
-
-              }
-              for (const h4 of h4s) {
-                console.log(`H4: ${$(h4).text()}`);
-              }
-            }
-
-            // Get the href from the a element inside the td
-            const a = $(td).find('a');
-            const href = a.attr('href');
-            const text = $(td).text().trim().replace(/\s+/g, ' ');
-            if (!isTitle && href && href.length > 0) {
-              console.log("p: " + href);
-              apartment.link = href;
-            } else if (!isTitle && text && text.length > 0) {
-              apartment.description = text;
-            }
-          }
-          apartments.push(apartment);
-          console.log();
-        }
-        console.log(apartments);
+        const apartments = await getApartments(listings);
 
         // Save the apartments to a file
         try {
           // TODO Save the file to a EXCEL file
-          await fs.writeFile(path.join(process.cwd(), 'apartments.json'), JSON.stringify(apartments, null, 2));
+          const apartmentsSorted = sortApartments(apartments);
+          const fileName = getFormattedDate(new Date()) + '_apartments.json';
+          await fs.writeFile(path.join(process.cwd(), 'apartments', fileName), JSON.stringify(apartmentsSorted, null, 2));
+
+          const apartmentsFiltered = filterApartments(apartmentsSorted);
+
+          // Append to the global.xls file the new apartments
+          const csvfileName = 'global.csv';
+          // Write each keys of the object to the file
+          for (const apartment of apartmentsFiltered) {
+            const { title, rent, rooms, roomSurface, street, travelTimeBikeToComedie, travelTimeBikeToPolytech, link, description } = apartment;
+            const line = `${title}\t${rent}\t${rooms}\t${roomSurface}\t${street}\t${travelTimeBikeToComedie}\t${travelTimeBikeToPolytech}\t${description}\t${link}\n`
+            await fs.appendFile(path.join(process.cwd(), 'apartments', csvfileName), line);
+          }
+
+          console.log('Apartments saved to file.')
+          console.log(`${apartmentsFiltered.length} interesting / ${apartments.length} apartments.`)
+
         } catch (error) {
           console.error(error);
         }
@@ -232,11 +324,27 @@ async function listMessages(auth) {
   });
 }
 
+function getFormattedDate(date) {
+  return date.toLocaleString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/\//g, '-');
+
+}
+
 async function readListOfAppartments() {
   try {
-    const apartments = await fs.readFile(path.join(process.cwd(), 'apartments.json'));
-    return JSON.parse(apartments);
-  } catch (_error) {
+
+    // Check if there is a file called YYYY-MM-DD_apartments.json
+
+    const fileName = getFormattedDate(new Date()) + '_apartments.json';
+    const buffer = await fs.readFile(path.join(process.cwd(), 'apartments', fileName));
+    let apartments = JSON.parse(buffer);
+    apartments = sortApartments(apartments);
+
+    const apartmentsFiltered = filterApartments(apartments);
+    console.log(apartmentsFiltered);
+
+    return apartments;
+  } catch (error) {
+    console.error('No apartments found. Fetching from email.' + error);
     authorize().then(listMessages).catch(console.error);
     return [];
   }
